41dc107679fe705ee6884d0c2530e64294e8649a
Andreas Junemann <ajunemann@outlook.de>
2020-06-12 21:15:31 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,67 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* ssid = "TP-Link_45C6"; // Enter your WiFi name
+const char* password =  "98273171"; // Enter WiFi password
+const char* mqttServer = "192.168.1.100";
+const int mqttPort = 1883;
 
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-#define LED 2
-int i = 0;
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED, OUTPUT);
-  Serial.begin(9600);
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
 }
+ 
 
-// the loop function runs over and over again forever
+void setup() {
+ 
+  Serial.begin(9600);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP8266Client")) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.publish("esp/test", "hello"); //Topic name
+  client.subscribe("esp/test");
+ 
+}
+ 
+
 void loop() {
-  i++;
-  Serial.println(i);
-  
-  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  client.loop();
 }
